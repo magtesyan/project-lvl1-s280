fac7ad4688ab36eb062de72556efaf3192244b1d
syan <magtesyan@inbox.ru>
2018-05-29 11:10:30 +0300

    29-05 11:10

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+
+const name = () => {
+	const imya = readlineSync.question('Welcome to the Brain Games!\n\nMay I have your name? ');
+	console.log('Hello, ' + imya);
+}
