f157d0661674eb61a0b3cda012c4ba6945883b33
syan <magtesyan@inbox.ru>
2018-05-29 11:36:03 +0300

    29.05 11:35

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'https://rawgit.com/anseki/readline-sync/master/lib/readline-sync.js';
 
 const name = () => {
 	const imya = readlineSync.question('Welcome to the Brain Games!\n\nMay I have your name? ');
 	console.log('Hello, ' + imya);
 }
+
+export default name;
