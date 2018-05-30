463ccffb3ef8a63ff58d971e90049f8ea87dbdfa
syan <magtesyan@inbox.ru>
2018-05-30 03:09:02 +0300

    30-05 3:08

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const name = () => {
-	const imya = readlineSync.question('Welcome to the Brain Games!\n\nMay I have your name? ');
-	console.log('Hello, ' + imya);
-}
+  const imya = readlineSync.question('Welcome to the Brain Games!\n\nMay I have your name? ');
+  console.log(`Hello, ${imya}`);
+};
 
 export default name;
