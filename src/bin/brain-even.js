aababaa2f08c0686f42584d86b6bae61be29cf01
syan <magtesyan@inbox.ru>
2018-05-31 17:50:37 +0300

    31-05-18 17:50

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
 import { braineven } from '..';
 
-const name1 = readlineSync.question('Welcome to the Brain Games!\n\nMay I have your name? ');
-braineven(0, name1);
+braineven();
--- src/index.js
+++ src/index.js
@@ -1,32 +1,61 @@
 import readlineSync from 'readline-sync';
 
+// 2 point
 const name = () => {
   const name0 = readlineSync.question('Welcome to the Brain Games!\n\nMay I have your name? ');
   console.log(`Hello, ${name0}`);
 };
 
+// WelcomeMessage - Welcome to the Brain Games!
+const welcomeMessage = () => console.log('Welcome to the Brain Games!\n');
 
-const braineven = (counter, name2) => {
-  let counter1 = counter;
-  const rnd = Math.round(Math.random() * 100);
-  let correctanswer = '';
-  console.log(`Question: ${rnd}`);
-  const answer = readlineSync.question('\nYour answer: ');
-  if (rnd % 2 === 0) correctanswer += 'yes';
-  else correctanswer += 'no';
-  if (correctanswer === answer) {
-    if (counter === 2) {
-      console.log(`Congratulations, ${name2}`);
-      return true;
-    }
+// TaskMessage
+const taskMessage = (game) => {
+  if (game === 'braineven') console.log('Answer "yes" if number even otherwise answer "no".\n\n');
+};
+
+// AskingName
+const getName = () => {
+  const playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${playerName}!`);
+  return playerName;
+};
+
+// RandomDigit
+const randomDigit = () => Math.round(Math.random() * 100);
+
+// IsEven
+const isEven = (digit) => {
+  if (digit % 2 === 0) return 'yes';
+  return 'no';
+};
+
+// GetQuestion
+const getQuestion = (game, digit1) => {
+  console.log('Question: ');
+  if (game === 'braineven') console.log(`${digit1}\n`);
+};
+
+// GetAnswer
+const getAnswer = (game, playerName) => {
+  let correctAnswer = '';
+  const digit1 = randomDigit();
+  if (game === 'braineven') correctAnswer += isEven(digit1);
+  for (let counter = 0; counter < 3; counter += 1) {
+    getQuestion(game, digit1);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer !== correctAnswer) return console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}`);
     console.log('Correct!');
-    counter1 += 1;
-    braineven(counter1, name2);
-  } else {
-    console.log(`${answer} is wrong answer ;(. Correct answer was ${correctanswer}\nLet's try again, ${name2}`);
-    return false;
   }
-  return true;
+  return console.log(`Congratulations,${playerName}!`);
+};
+
+// 4 point
+const braineven = () => {
+  welcomeMessage();
+  taskMessage('braineven');
+  const playerName = getName();
+  getAnswer('braineven', playerName);
 };
 
 export { braineven, name };
