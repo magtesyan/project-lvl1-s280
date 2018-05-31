c0ad9aa2abe59ddca2e4d0eed63c956ffb0c3d40
syan <magtesyan@inbox.ru>
2018-05-31 21:47:16 +0300

    31-05-18 21:47

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ const welcomeMessage = () => console.log('Welcome to the Brain Games!');
 // TaskMessage
 const taskMessage = (game) => {
   if (game === 'braineven') console.log('Answer "yes" if number even otherwise answer "no".\n');
+  if (game === 'calculator') console.log('What is the result of the expression?\n');
 };
 
 // AskingName
@@ -31,17 +32,35 @@ const isEven = (digit) => {
 };
 
 // GetQuestion
-const getQuestion = (game, digit1) => {
+const getQuestion = (game, digit1, digit2, operator) => {
   if (game === 'braineven') console.log(`Question: ${digit1}`);
+  if (game === 'calculator') console.log(`Question: ${digit1} ${operator} ${digit2}`);
 };
 
 // GetAnswer
 const getAnswer = (game, playerName) => {
   for (let counter = 0; counter < 3; counter += 1) {
     let correctAnswer = '';
+    let operator = '';
     const digit1 = randomDigit();
+    const digit2 = randomDigit();
+    const digit3 = randomDigit();
     if (game === 'braineven') correctAnswer += isEven(digit1);
-    getQuestion(game, digit1);
+    if (game === 'calculator') {
+      if (digit3 < 33) {
+        correctAnswer = digit1 + digit2;
+        operator += '+';
+      }
+      if (digit3 < 66) {
+        correctAnswer = digit1 - digit2;
+        operator += '-';
+      }
+      if (digit3 < 101) {
+        correctAnswer = digit1 * digit2;
+        operator += '*';
+      }
+    }
+    getQuestion(game, digit1, digit2, operator);
     const answer = readlineSync.question('Your answer: ');
     if (answer !== correctAnswer) return console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}`);
     console.log('Correct!\n');
@@ -57,4 +76,12 @@ const braineven = () => {
   getAnswer('braineven', playerName);
 };
 
-export { braineven, name };
+// 5 point
+const calculator = () => {
+  welcomeMessage();
+  taskMessage('calculator');
+  const playerName = getName();
+  getAnswer('braineven', playerName);
+};
+
+export { braineven, name, calculator };
