39d69b378541d54f14ab121c49adb4ebbc57d33e
syan <magtesyan@inbox.ru>
2018-06-02 18:00:18 +0300

    02-06-18 18:00

--- src/games/braincalculator-game-code.js
+++ src/games/braincalculator-game-code.js
@@ -8,4 +8,20 @@ const braincalculator = () => {
   getAnswer('braincalculator', playerName);
 };
 
-export default braincalculator;
+const getOpResult = (digit1, digit2, digit3) => {
+  let correctAnswer = '';
+  let operator = '';
+  if (digit3 < 33) {
+    correctAnswer += digit1 + digit2;
+    operator += '+';
+  } else if (digit3 < 66) {
+    correctAnswer += digit1 - digit2;
+    operator += '-';
+  } else {
+    correctAnswer += digit1 * digit2;
+    operator += '*';
+  }
+  return [correctAnswer, operator];
+};
+
+export { braincalculator, getOpResult };
