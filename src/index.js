cff998e3feaf90376289ab1b4cb62fec82afbaa8
syan <magtesyan@inbox.ru>
2018-06-04 13:05:23 +0300

    04-06-2018 13:05

--- src/index.js
+++ src/index.js
@@ -6,9 +6,12 @@ const engine = (taskMessage, startRound) => {
   console.log(`Hello ${playerName}!\n`);
 
   for (let counter = 0; counter < 3; counter += 1) {
-    console.log(startRound()[0]);
+    const gameParameters = [];
+    gameParameters[0] = startRound();
+    gameParameters[1] = startRound();
+    console.log(gameParameters[0]);
     const answer = readlineSync.question('Your answer: ');
-    if (answer !== startRound()[1]) return console.log(`${answer} is wrong answer ;(. Correct answer was ${startRound()[1]}`);
+    if (answer !== gameParameters[1]) return console.log(`${answer} is wrong answer ;(. Correct answer was ${gameParameters[1]}`);
     console.log('Correct!\n');
   }
   return console.log(`Congratulations, ${playerName}!`);
