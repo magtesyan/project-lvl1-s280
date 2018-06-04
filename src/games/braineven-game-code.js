48760b78a93113cc45bc9f07cddc154be25aa895
syan <magtesyan@inbox.ru>
2018-06-04 13:25:44 +0300

    04-06-2018 13:25

--- src/games/braineven-game-code.js
+++ src/games/braineven-game-code.js
@@ -8,16 +8,16 @@ const isEven = (digit) => {
   return 'no';
 };
 
+const startRound = () => {
+  const digit1 = getRandomDigit();
+  const question = (`Question: ${digit1}`);
+  const correctAnswer = isEven(digit1);
+  return [question, correctAnswer];
+};
+
 const braineven = () => {
-  const correctAnswer = [];
-  const question = [];
   const task = ('Answer "yes" if number even otherwise answer "no".');
-  for (let counter = 0; counter < 3; counter += 1) {
-    const digit1 = getRandomDigit();
-    question[counter] = (`Question: ${digit1}`);
-    correctAnswer[counter] = isEven(digit1);
-  }
-  engine(task, question, correctAnswer);
+  engine(task, startRound);
 };
 
 export default braineven;
