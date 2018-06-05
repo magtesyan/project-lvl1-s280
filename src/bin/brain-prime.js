8bd3797c1141f27d785214a4f1ce11ed9d993931
syan <magtesyan@inbox.ru>
2018-06-05 03:50:39 +0300

    05-06-2018 3:50

---
+++ src/bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import brainprime from '../games/prime';
+
+brainprime();
---
+++ src/games/prime.js
@@ -0,0 +1,25 @@
+// 9 point
+import engine from '..';
+import getRandomNumber from '../utils';
+
+const task = ('Is this number prime?');
+
+const isPrime = (randomDigit) => {
+  for (let counter = 2; counter < randomDigit; counter += 1) {
+    if (randomDigit % counter === 0) return false;
+  }
+  return true;
+};
+
+const startRound = () => {
+  const randomDigit = getRandomNumber(100);
+  const question = (`${randomDigit}`);
+  const correctAnswer = (isPrime(randomDigit)) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+const brainprime = () => {
+  engine(task, startRound);
+};
+
+export default brainprime;
