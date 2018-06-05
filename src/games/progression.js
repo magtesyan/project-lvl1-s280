4c67b11ac69398bf2f9f3f617fa6ad2f74b5ce80
syan <magtesyan@inbox.ru>
2018-06-05 13:33:15 +0300

    05-06-2018 13:33

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,19 +4,24 @@ import getRandomNumber from '../utils';
 
 const task = ('What number is missing in this progression?');
 const progressionLength = 10;
-const progressionArrow = [progressionLength];
-progressionArrow[0] = getRandomNumber(10);
-const progrType = getRandomNumber(2);
-const progrStep = getRandomNumber(3);
-for (let counter = 1; counter < progressionLength; counter += 1) {
-  if (progrType === 1) progressionArrow[counter] = progressionArrow[counter - 1] + progrStep;
-  else progressionArrow[counter] = progressionArrow[counter - 1] * progrStep;
-}
+let progressionArrow = [progressionLength];
+
+const getProgression = () => {
+  progressionArrow[0] = getRandomNumber(10);
+  const progrType = getRandomNumber(2);
+  const progrStep = getRandomNumber(3);
+  for (let counter = 1; counter < progressionLength; counter += 1) {
+    if (progrType === 1) progressionArrow[counter] = progressionArrow[counter - 1] + progrStep;
+    else progressionArrow[counter] = progressionArrow[counter - 1] * progrStep;
+  }
+  return progressionArrow;
+};
 
 const gameData = () => {
   let correctAnswer = 0;
   let question = '';
   const randomNumber = getRandomNumber(10);
+  progressionArrow = getProgression();
   for (let counter = 0; counter < progressionLength; counter += 1) {
     if (counter === randomNumber) {
       question += '.. ';
