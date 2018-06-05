3312a9161eac2aa16d81a83fe7608c61f081dbf9
syan <magtesyan@inbox.ru>
2018-06-05 03:27:48 +0300

    05-06-2018 3:27

--- src/games/calculator.js
+++ src/games/calculator.js
@@ -7,13 +7,13 @@ const task = ('What is the result of the expression?');
 const getOpResult = (randomDigit1st, randomDigit2nd, operator) => {
   let correctAnswer = 0;
   switch (operator) {
-    case '1':
+    case '+':
       correctAnswer += randomDigit1st + randomDigit2nd;
       break;
-    case '2':
+    case '-':
       correctAnswer -= randomDigit1st + randomDigit2nd;
       break;
-    case '3':
+    case '*':
       correctAnswer *= randomDigit1st + randomDigit2nd;
       break;
     default:
@@ -26,10 +26,21 @@ const startRound = () => {
   const randomDigit1st = getRandomNumber(100);
   const randomDigit2nd = getRandomNumber(100);
   const randomDigit3rd = getRandomNumber(3);
+  console.log(randomDigit3rd);
   let operator = '';
-  if (randomDigit3rd < 333) operator += '+';
-  else if (randomDigit3rd < 666) operator += '-';
-  else operator += '*';
+  switch (randomDigit3rd) {
+    case '1':
+      operator += '+';
+      break;
+    case '2':
+      operator += '-';
+      break;
+    case '3':
+      operator += '*';
+      break;
+    default:
+      break;
+  }
   const correctAnswer = String(getOpResult(randomDigit1st, randomDigit2nd, operator));
   const question = (`${randomDigit1st} ${operator} ${randomDigit2nd}`);
   return [question, correctAnswer];
